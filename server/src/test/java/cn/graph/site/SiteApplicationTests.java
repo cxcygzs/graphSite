bcb78b0e193c0d5e7548e6e7dc01f22f0fe32a52
wfy <1335680234@qq.com>
2022-12-01 21:04:03 +0800

    add: 初始化java程序

---
+++ server/src/test/java/cn/graph/site/SiteApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.graph.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
