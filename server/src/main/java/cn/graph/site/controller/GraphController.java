fbec14cc5e3573fd2dee1f7b56fb297240387d29
wfy <1335680234@qq.com>
2022-12-01 21:15:49 +0800

    add: 提交controller

---
+++ server/src/main/java/cn/graph/site/controller/GraphController.java
@@ -0,0 +1,13 @@
+package cn.graph.site.controller;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * {@code @author:} wfy
+ * {@code @date:} 2022/12/1
+ **/
+@RestController
+@ResponseBody
+public class GraphController {
+}
